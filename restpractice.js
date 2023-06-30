8896989e76e7b01cb54de2c238f4ce6026d7683f
ManojNadar <manojviru03@gmail.com>
2023-06-30 23:00:30 +0530

    rest practice

---
+++ restpractice.js
@@ -0,0 +1,13 @@
+// function addNum(a, b, ...rest) {
+//   return rest;
+// }
+
+// console.log(addNum(2, 3, 5, 8, 9, 10));
+
+// let arr = [5, 6, 8, 10];
+// let [a, b, ...rest] = arr;
+
+// // console.log(a);
+// console.log(rest);
+
+
